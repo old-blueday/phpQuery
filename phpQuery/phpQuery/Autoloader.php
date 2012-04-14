a49152fef794890b1f5d66d5e7de818c729fdb8f
bluelovers <codelovers@users.sourceforge.net>
2012-04-15 00:14:32 +0800

    phpQuery_Autoloader

---
+++ phpQuery/phpQuery/Autoloader.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * @author bluelovers
+ * @copyright 2012
+ */
+
+class phpQuery_Autoloader implements Zend_Loader_Autoloader_Interface
+{
+	protected $_baseDir;
+	protected $_prefix;
+	protected $_prefixLen;
+
+	public function __construct($baseDir, $prefix)
+	{
+		$this->_baseDir = $baseDir;
+		$this->_prefix = $prefix;
+		$this->_prefixLen = strlen($prefix);
+	}
+
+	public function autoload($class)
+	{
+		if (strlen($class) <= $this->_prefixLen)
+		{
+			return false;
+		}
+		if (0 !== strpos($class, $this->_prefix))
+		{
+			return false;
+		}
+
+		$fragment = substr($class, $this->_prefixLen);
+		$fragment = str_replace('_', '/', trim($fragment, '_'));
+		return include ($this->_baseDir . '/' . $fragment . '.php');
+	}
+}
