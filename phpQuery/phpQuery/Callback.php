f61e8dfdfb3884a90976d2755c3706334efae0a6
bluelovers <codelovers@users.sourceforge.net>
2012-04-18 05:04:00 +0800

    分離 CallbackReference - 此類別未定義

--- phpQuery/phpQuery/Callback.php
+++ phpQuery/phpQuery/Callback.php
@@ -56,14 +56,4 @@ class Callback
 //	}
 }
 
-//class CallbackReference extends Callback {
-//	/**
-//	 *
-//	 * @param $reference
-//	 * @param $paramIndex
-//	 * @todo implement $paramIndex; param index choose which callback param will be passed to reference
-//	 */
-//	public function __construct(&$reference, $name = null){
-//		$this->callback =& $reference;
-//	}
-//}
+
---
+++ phpQuery/phpQuery/CallbackReference.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * @author bluelovers
+ * @copyright 2012
+ */
+
+//class CallbackReference extends Callback
+//{
+//	/**
+//	 *
+//	 * @param $reference
+//	 * @param $paramIndex
+//	 * @todo implement $paramIndex; param index choose which callback param will be passed to reference
+//	 */
+//	public function __construct(&$reference, $name = null)
+//	{
+//		$this->callback = &$reference;
+//	}
+//}
