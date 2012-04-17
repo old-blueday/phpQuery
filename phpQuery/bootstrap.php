225f22490300ed9470a5c70e92be9c78f76662ff
bluelovers <codelovers@users.sourceforge.net>
2012-04-18 04:00:33 +0800

    簡化代碼

--- phpQuery/bootstrap.php
+++ phpQuery/bootstrap.php
@@ -5,8 +5,12 @@
  * @copyright 2012
  */
 
+//set_include_path(get_include_path() . PATH_SEPARATOR . 'D:/Users/Documents/The Project/ZendFramework-1.x/library/');
+
 require_once('Zend/Loader/Autoloader.php');
-require_once(dirname(__FILE__).'/phpQuery/Autoloader.php');
+
+Zend_Loader_Autoloader::getInstance();
+Zend_Loader::loadClass('phpQuery_Autoloader', dirname(__FILE__));
 
 Zend_Loader_Autoloader::getInstance()
 	->pushAutoloader(phpQuery_Autoloader::getInstance(dirname(__FILE__), 'phpQuery'));
