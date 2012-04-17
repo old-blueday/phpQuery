cec32648c0d7857be829daaddcdd0ec79d3cb069
bluelovers <codelovers@users.sourceforge.net>
2012-04-18 05:55:44 +0800

    分離 除錯用代碼

--- phpQuery/bootstrap.php
+++ phpQuery/bootstrap.php
@@ -5,11 +5,6 @@
  * @copyright 2012
  */
 
-set_include_path(get_include_path() . PATH_SEPARATOR . 'D:/Users/Documents/The Project/ZendFramework-1.x/library/');
-
-$_get_declared_classes = get_declared_classes();
-$_get_declared_interfaces = get_declared_interfaces();
-
 require_once('Zend/Loader/Autoloader.php');
 
 Zend_Loader_Autoloader::getInstance();
@@ -18,34 +13,3 @@ Zend_Loader::loadClass('phpQuery_Autoloader', dirname(__FILE__));
 Zend_Loader_Autoloader::getInstance()
 	->pushAutoloader(phpQuery_Autoloader::getInstance(dirname(__FILE__), 'phpQuery'));
 ;
-
-$class_check = array(
-	'phpQuery_Autoloader',
-	'phpQuery',
-	'phpQuery_Dom_DOMEvent',
-	'phpQuery_Dom_DOMDocumentWrapper',
-	'phpQuery_Events',
-	'phpQuery_Callback_Param',
-	'phpQuery_Callback',
-	'phpQuery_Callback_Body',
-	'phpQuery_Callback_ReturnReference',
-	'phpQuery_Callback_ReturnValue',
-	'phpQuery_Callback_ParameterToReference',
-	'phpQuery_Plugins',
-	'phpQuery_Object',
-
-	'phpQuery_Callback_NamedInterface',
-);
-
-$r = array();
-foreach($class_check as $_c)
-{
-	$r[$_c] = class_exists($_c) || interface_exists($_c);
-}
-
-echo '<pre>';
-var_dump($r);
-
-var_export(array_diff(get_declared_classes(), $_get_declared_classes, $class_check));
-var_export(array_diff(get_declared_interfaces(), $_get_declared_interfaces, $class_check));
-echo '</pre>';
