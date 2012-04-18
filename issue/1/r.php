7ea4f33e47b8097699b330353dbb00ceae0735fc
bluelovers <codelovers@users.sourceforge.net>
2012-04-19 06:13:26 +0800

    .

---
+++ issue/1/r.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @author bluelovers
+ * @copyright 2012
+ */
+
+require_once(dirname(__file__).'/../../phpQuery/bootstrap.php');
+
---
+++ issue/bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * @author bluelovers
+ * @copyright 2012
+ */
+
+require_once (dirname(__file__) . '/../phpQuery/bootstrap.php');
