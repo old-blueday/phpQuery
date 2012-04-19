7c8119dcae1b84d8c71ac8ef6e600d3cc42b9cf6
bluelovers <codelovers@users.sourceforge.net>
2012-04-19 11:58:21 +0800

    .

---
+++ unit-tests/bootstrap.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * @author Ower
+ */
+// TODO: check include path
+//ini_set('include_path', ini_get('include_path'));
+
+// put your code here
+
+require_once '../phpQuery/bootstrap.php';
+
+
+?>
